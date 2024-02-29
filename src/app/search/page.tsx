bf0d319b4966f0194e6da615df963533a53d3705
mkowack <kowackag@gmail.com>
2024-03-01 00:35:12 +0100

    Title search phrase

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,6 +5,8 @@ import { SectionContainer } from "@/ui/atoms/SectionContainer";
 import { Title } from "@/ui/atoms/Title";
 
 import { getProductsByQuery } from "@/api/products";
+// import { Suspense } from "react";
+// import { Loading } from "@/ui/atoms/Loading";
 
 type ProductSearchPageType = {
 	// readonly params: { page: string };
@@ -22,10 +24,12 @@ export default async function ProductsPage({
 
 	return (
 		<SectionContainer>
-			<Title>Searching: </Title>
+			<Title>{`Products for phrase: ${query}`}</Title>
 			{products.length ? (
+				// <Suspense fallback={<Loading />}>
 				<ProductList products={products} />
 			) : (
+				// </Suspense>
 				<p>{`No results for phrase: ${query}`}</p>
 			)}
 		</SectionContainer>
