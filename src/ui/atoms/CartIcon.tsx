44a934a44bf91f12fefc61f826419ceeacba03d3
mkowack <kowackag@gmail.com>
2024-04-08 20:02:58 +0200

    CartIcon

--- src/ui/atoms/CartIcon.tsx
+++ src/ui/atoms/CartIcon.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+
 import { cookies } from "next/headers";
 import { Paths } from "@/paths";
 import { getCartById } from "@/api/cart";
@@ -20,7 +21,7 @@ export const CartIcon = async () => {
 
 	const cart = await getCartById(cartId);
 
-	const quantity = cart?.items.reduce((acc:number, product) => {
+	const quantity = cart?.items.reduce((acc: number, product) => {
 		return acc + product.quantity;
 	}, 0);
 
@@ -35,7 +36,7 @@ export const CartIcon = async () => {
 					<use href="/icons.svg#shoping-card"></use>
 				</svg>
 
-				<p className="absolute bottom-3 right-0 mr-2 h-5 w-5 rounded-full bg-rose-700 py-[2px] text-center align-middle text-xs text-neutral-100">
+				<p className="absolute bottom-3 right-0 mr-2 h-5 w-5 rounded-full bg-rose-700 py-0.5 text-center align-middle text-xs text-neutral-100">
 					{quantity}
 				</p>
 			</div>
